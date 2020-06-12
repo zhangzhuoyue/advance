626a62f22d3fc521d991e8921d30e5542ebabb75
zy <szy6481@163.com>
2020-06-12 21:50:55 +0800

    封装数据库配置和sql的两个bean对象

--- mybatis/IPersence/src/main/java/pojo/Configuration.java
+++ mybatis/IPersence/src/main/java/pojo/Configuration.java
@@ -1,4 +1,29 @@
 package pojo;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Configuration {
+
+    //数据源
+    DataSource dataSource;
+    //封装sql集合
+    Map<String,MapperStatement> statement = new HashMap<>();
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Map<String, MapperStatement> getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Map<String, MapperStatement> statement) {
+        this.statement = statement;
+    }
 }
