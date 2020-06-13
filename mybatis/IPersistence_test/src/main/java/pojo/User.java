da84585d8a66a2c95ede512450c3f379ad80f4c6
zy <szy6481@163.com>
2020-06-13 23:07:29 +0800

    自定义框架测试

---
+++ mybatis/IPersistence_test/src/main/java/pojo/User.java
@@ -0,0 +1,64 @@
+package pojo;
+
+/**
+ * @author zzy
+ * @date 2020/6/13 10:38
+ */
+public class User {
+
+    /**
+     * mybatis使用反射和内省技术，将数据库字段和实体对象中属性进行封装
+     * 如果使用mybatis默认的映射封装技术，则数据库查询出的字段名要和实体对象属性相同【本质反射技术在复制使用setxx方法，只要set后面的名字和数据库匹配即可】，否在无法映射，表现为成员属性一直是默认值
+     * 对象的成员类型要和数据库字段类型对应，否则报错java.lang.IllegalArgumentException: argument type mismatch，【本质原因是通过invoke调用setxx方法进行赋值值参数类型不同】
+     */
+
+    public int id;
+
+    public String username;
+
+    public String passaaword;
+
+    public String birtha2dy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return passaaword;
+    }
+
+    public void setPassword(String password) {
+        this.passaaword = password;
+    }
+
+    public String getBirthady() {
+        return birtha2dy;
+    }
+
+    public void setBirthady(String birthady) {
+        this.birtha2dy = birthady;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + passaaword + '\'' +
+                ", birthady='" + birtha2dy + '\'' +
+                '}';
+    }
+}
--- mybatis/IPersistence_test/src/main/java/test/IPerenceTest.java
+++ mybatis/IPersistence_test/src/main/java/test/IPerenceTest.java
@@ -1,12 +1,35 @@
 package test;
 
+import config.XMLConfigBuilder;
 import io.IResources;
+import org.dom4j.DocumentException;
+import pojo.Configuration;
+import pojo.User;
+import sqlSession.SqlSession;
+import sqlSession.SqlSessionFactory;
+import sqlSession.SqlSessionFactoryBuilder;
 
+import java.beans.PropertyVetoException;
 import java.io.InputStream;
+import java.util.List;
 
 public class IPerenceTest {
 
-    public static void main(String[] args) {
-        InputStream resourceAsStream = IResources.getResourceAsStream("");
+    public static void main(String[] args) throws Exception {
+        //获取文件输入流
+        InputStream resourceAsStream = IResources.getResourceAsStream("/databaseConfig.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSqlSession();
+
+        List<Object> objects = sqlSession.selectList("product.selectList");
+
+        System.out.println(objects);
+        System.out.println("---------------------");
+        User user = new User();
+        user.setId(3);
+        user.setUsername("zhang");
+        Object o = sqlSession.selectOne("product.selectOne",user);
+        System.out.println(o);
     }
 }
