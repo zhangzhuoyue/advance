78422af9e5a7506dd3dbfa1de2605d9eaa2942e8
zy <szy6481@163.com>
2020-06-13 23:03:08 +0800

    BoundSql:将#{}替换？，将#{}取出并存储 Configuration 封装数据配置 ParamsMapping 封装sql

--- mybatis/IPersence/src/main/java/pojo/Configuration.java
+++ mybatis/IPersence/src/main/java/pojo/Configuration.java
@@ -7,9 +7,9 @@ import java.util.Map;
 public class Configuration {
 
     //数据源
-    DataSource dataSource;
-    //封装sql集合
-    Map<String,MapperStatement> statement = new HashMap<>();
+    public DataSource dataSource;
+    //key：statementId  value  :MapperStatement
+    public  Map<String,MapperStatement> statement = new HashMap<>();
 
     public DataSource getDataSource() {
         return dataSource;
