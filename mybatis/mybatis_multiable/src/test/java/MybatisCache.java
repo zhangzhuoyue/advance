f01b60a52422555608c2c664e1261157459820da
zy <szy6481@163.com>
2020-06-18 22:39:43 +0800

    测试mybatis的以及缓存，缓存失效情况

---
+++ mybatis/mybatis_multiable/src/test/java/MybatisCache.java
@@ -0,0 +1,37 @@
+import com.zzy.dao.UserMapperDao;
+import com.zzy.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author zzy
+ * @date 2020/6/18 22:33
+ */
+public class MybatisCache {
+
+    public SqlSession sqlSession;
+    @Before
+    public void before() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlConfig.xml");
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        sqlSession = build.openSession(true);
+    }
+
+    @Test
+    public void firstCache(){
+        UserMapperDao mapper = sqlSession.getMapper(UserMapperDao.class);
+        User userOrders = mapper.findUserOrders(4);
+        sqlSession.clearCache();
+        User userOrders1 = mapper.findUserOrders(4);
+
+        System.out.println(userOrders == userOrders1);
+
+    }
+}
